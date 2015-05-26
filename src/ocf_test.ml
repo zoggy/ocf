e189e5ee14f3cf47fdf9bacf4da398edba617a81
Zoggy <zoggy@bat8.org>
2015-05-26 14:33:38 +0200

    more test

--- src/ocf_test.ml
+++ src/ocf_test.ml
@@ -1,6 +1,6 @@
 (** *)
 
-let int_opt = Ocf.int ~desc:"this is x" 
+let int_opt = Ocf.int ~desc:"this is x"
   ~cb: (fun n -> prerr_endline ("x="^(string_of_int n))) 0
 let float_opt = Ocf.float 3.14
 let string_opt = Ocf.string ~cb: prerr_endline "hello world!"
@@ -18,3 +18,7 @@ let root = Ocf.add_group root ["foo" ; "bar"] group
 let str = Ocf.to_string root
 let () = print_endline str
 let () = Ocf.from_string root str
+
+let () =
+  try ignore(Ocf.add root ["foo" ; "bar" ; "x"] int_opt)
+  with Ocf.Error e -> prerr_endline (Ocf.string_of_error e)
